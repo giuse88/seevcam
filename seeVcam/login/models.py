600372f41fc87e00305f1149751b38569e2cb5d8
fadykalo <fadykalo@gmail.com>
2014-06-21 20:20:14 +0100

    fixed bug in the model

--- seeVcam/login/models.py
+++ seeVcam/login/models.py
@@ -1,7 +1,15 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
-
+from allauth.account.models import EmailAddress
 
 class SeevUser(AbstractUser):
     company = models.CharField(_("company"), max_length=200, null=True, blank=True)
+
+    def account_verified(self):
+        if self.is_authenticated:
+            result = EmailAddress.objects.filter(email=self.email)
+            if len(result):
+                return result[0].verified
+        return False
+
