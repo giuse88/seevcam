04530a0d0f6bc9423e4208c27016e740b08f4b13
fadykalo <fadykalo@gmail.com>
2014-06-23 21:50:07 +0100

    fix toolbar and changed profile url

--- seeVcam/login/urls.py
+++ seeVcam/login/urls.py
@@ -7,6 +7,6 @@ urlpatterns = patterns('',
 
                        # allauth package doesn't provide e profile page
                        url(r'^$', views.home, name='home'),
-                       url(r'^accounts/profile/$', views.welcome, name='profile'),
+                       url(r'^accounts/profile/$', views.welcome, name='account_profile'),
                        url(r'^accounts/only_verified/$', views.verified_users_only_view, name='only_virified'),
 )
