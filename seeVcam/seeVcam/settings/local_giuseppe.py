22e3851075bd8769608f5aa3a6ad8bb9edf26fb7
Giuseppe <giuse88@gmail.com>
2014-07-07 19:59:25 +0100

    My local configuration.

---
+++ seeVcam/seeVcam/settings/local_giuseppe.py
@@ -0,0 +1,29 @@
+# settings.local.py
+from .base import *
+
+# Debug options
+DEBUG = True
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+TEMPLATE_DEBUG = True
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'seevcamdb',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+    }
+}
+
+INSTALLED_APPS += ("debug_toolbar",)
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+# allauth settings
+
+# Determines whether or not an e-mail address is automatically confirmed by a mere GET request. default(=False)
+ACCOUNT_CONFIRM_EMAIL_ON_GET= True
