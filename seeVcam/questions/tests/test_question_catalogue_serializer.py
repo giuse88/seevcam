c20350757f09a06d6f411dde6a2eac758d54d7c0
Giuseppe <giuse88@gmail.com>
2014-07-13 09:50:04 +0100

    SEEV-39 Create QuestionCatalogue Serializer ( question serializer )

---
+++ seeVcam/questions/tests/test_question_catalogue_serializer.py
@@ -0,0 +1,50 @@
+from django.test import TestCase
+from rest_framework.compat import BytesIO
+from rest_framework.parsers import JSONParser
+from rest_framework.renderers import JSONRenderer
+
+from questions.serializers import QuestionSerializer
+from questions.models import Question, QuestionCatalogue
+
+
+class QuestionSerializerTest(TestCase):
+    ##############################################################################
+    #                                PROPERTIES                                  #
+    ##############################################################################
+
+    # The json must be in strict mode
+    question_catalogue = None
+
+    ##############################################################################
+    #                                SETTING UP                                  #
+    ##############################################################################
+
+    def setUp(self):
+        self.question_catalogue = QuestionCatalogue(pk=1, catalogue_scope='private', catalogue_name='test')
+        self.question_catalogue.save()
+
+    ##############################################################################
+    #                                PUBLIC TESTS                                #
+    ##############################################################################
+
+    def test_question_catalogue_deserializer_valid_json(self):
+        catalogue_json = '{"id": 2, "catalogue_scope": "SEEVCAM", "catalogue_name": "cat"}'
+        stream = BytesIO(catalogue_json)
+        data = JSONParser().parse(stream)
+        question_catalogue_serializer = QuestionSerializer(data=data)
+        self.assertFalse(question_catalogue_serializer.is_valid())
+
+
+    def test_question_catalogue_deserializer_invalid_json(self):
+        pass
+
+    def test_question_catalogue_serializer(self):
+        pass
+
+    def test_question_catalogue_invalid_scope(self):
+        catalogue_json = '{"id": 2, "catalogue_scope": "wrong", "catalogue_name": "cat"}'
+        stream = BytesIO(catalogue_json)
+        data = JSONParser().parse(stream)
+        question_catalogue_serializer = QuestionSerializer(data=data)
+        self.assertFalse(question_catalogue_serializer.is_valid())
+
