7e6260c849b31ed81bb093f2e073e5d48c7460c5
fadykalo <fadykalo@gmail.com>
2014-07-14 19:24:19 +0100

    changes to the settings file local_fady

--- seeVcam/seeVcam/settings/local_fady.py
+++ seeVcam/seeVcam/settings/local_fady.py
@@ -4,9 +4,9 @@ from .base import *
 # Debug options
 DEBUG = True
 
-SECRET_KEY = 'locale_fake_key^8m2v$*7^9qp$4z6r2%3o8w34gox3u)^^%4%fe1*@'
-
 TEMPLATE_DEBUG = True
+
+SECRET_KEY = 'locale_fake_key^8m2v$*7^9qp$4z6r2%3o8w34gox3u)^^%4%fe1*@'
 
 DATABASES = {
     'default': {
@@ -16,12 +16,9 @@ DATABASES = {
         'PASSWORD': '',
         'HOST': '127.0.0.1',
         'PORT': '5432',
-    }
+        }
 }
-
-INSTALLED_APPS += ("debug_toolbar",)
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
-# Determines whether or not an e-mail address is automatically confirmed by a mere GET request. default(=False)
-ACCOUNT_CONFIRM_EMAIL_ON_GET= True
+ACCOUNT_CONFIRM_EMAIL_ON_GET = True
