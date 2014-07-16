f48f00dd77e2de328eda304104f5c174d393b2e6
Giuseppe <giuse88@gmail.com>
2014-07-16 12:21:39 +0100

    SEEV-46 Implement admin page for seevcam catalogue

--- seeVcam/questions/admin.py
+++ seeVcam/questions/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
-# Register your models here.
+from models import QuestionCatalogue, Question
+
+
+class QuestionCatalogueAdmin(admin.ModelAdmin):
+    fields = ('catalogue_name',)
+
+    def save_model(self, request, obj, form, change):
+        obj.catalogue_owner = request.user
+        obj.catalogue_scope = QuestionCatalogue.SEEVCAM_SCOPE
+        obj.save()
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ('question_text', 'question_catalogue',)
+
+
+admin.site.register(QuestionCatalogue, QuestionCatalogueAdmin)
+admin.site.register(Question, QuestionAdmin)
