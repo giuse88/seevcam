abb26d80e9aa229dde054ea480b73b62c821234f
fadykalo <fadykalo@gmail.com>
2014-07-19 16:21:29 +0100

    fixed wrong decorator @login_required applied to dashboard.view

--- seeVcam/dashboard/views.py
+++ seeVcam/dashboard/views.py
@@ -2,6 +2,5 @@ from django.contrib.auth.decorators import login_required
 from django.views.generic import TemplateView
 
 
-@login_required
 class DashboardView(TemplateView):
     template_name = "dashboard.html"
