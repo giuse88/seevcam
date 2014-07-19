50f73fae93bb7de90997a6189098d5d2fdda7b0e
fadykalo <fadykalo@gmail.com>
2014-07-19 18:41:11 +0100

    SEEV-56 Create Model for the User

--- seeVcam/authentication/models.py
+++ seeVcam/authentication/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
 
 class SeevcamUser(AbstractUser):
-    pass
+    job_title = models.CharField(max_length=255, null=False, blank=False)
