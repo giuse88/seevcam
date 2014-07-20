2b31a7fd4d830d296b59ed463355c46cfe654940
Giuseppe <giuse88@gmail.com>
2014-07-20 10:49:39 +0100

    SEEV-1 Create authentication mechanism ( inserted required login )

--- seeVcam/dashboard/urls.py
+++ seeVcam/dashboard/urls.py
@@ -1,13 +1,13 @@
 from django.conf.urls import patterns, include, url
 from django.conf import settings
+from django.contrib.auth.decorators import login_required
 
 from views import DashboardView
 
-
 urlpatterns = patterns('',
-                       url(r'^logout/', 'django.contrib.auth.views.logout', {'next_page': settings.LOGIN_URL},
+                       url(r'^logout/', login_required('django.contrib.auth.views.logout'), {'next_page': settings.LOGIN_URL},
                            name="logout"),
                        url(r'^interviews/', include('interviews.urls')),
                        url(r'^questions/', include('questions.urls')),
-                       url(r'^$', DashboardView.as_view(), name='dashboard'),
+                       url(r'^$', login_required(DashboardView.as_view()), name='dashboard'),
 )
