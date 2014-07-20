6c6ac733abab9281c530d1745382634790add3a1
Giuseppe <giuse88@gmail.com>
2014-07-20 12:37:48 +0100

    SEEV-1 Create authentication mechanism (better solution for logout mechanism )

--- seeVcam/authentication/views.py
+++ seeVcam/authentication/views.py
@@ -1,10 +1,7 @@
 # NOT TO BE USED IN PRODUCTION
-from django.contrib.auth.decorators import login_required
 from django.contrib.auth.views import login
 from django.shortcuts import redirect
 from django.conf import settings
-from django.contrib.auth.views import logout_then_login
-
 
 def login_or_redirect(request, **kwargs):
     if request.user.is_authenticated():
@@ -12,7 +9,3 @@ def login_or_redirect(request, **kwargs):
     else:
         return login(request, **kwargs)
 
-
-@login_required
-def protected_logout(request, **kwargs):
-    return logout_then_login(request, **kwargs)
