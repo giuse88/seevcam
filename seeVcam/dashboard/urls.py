c7c0fd70b11e2c4806eab92e460e1c4526118bc4
Giuseppe <giuse88@gmail.com>
2014-07-20 12:33:08 +0100

    SEEV-1 Create authentication mechanism ( fixed logout issue)

--- seeVcam/dashboard/urls.py
+++ seeVcam/dashboard/urls.py
@@ -1,14 +1,10 @@
 from django.conf.urls import patterns, include, url
-from django.conf import settings
-from django.contrib.auth.decorators import login_required
-
+from authentication.views import protected_logout
 from views import DashboardView
 
 
 urlpatterns = patterns('',
-                       url(r'^logout/', login_required('django.contrib.auth.views.logout'),
-                           {'next_page': settings.LOGIN_URL},
-                           name="logout"),
+                       url(r'^logout/', protected_logout, name="logout"),
                        url(r'^interviews/', include('interviews.urls')),
                        url(r'^questions/', include('questions.urls')),
                        url(r'^$', DashboardView.as_view(), name='dashboard'),
