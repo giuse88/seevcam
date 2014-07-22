9f7871dc968e0b903c973b6bbfe95d6f95395ea7
fadykalo <fadykalo@gmail.com>
2014-07-22 22:57:51 +0100

    SEEV-61 Implement form for the userprofile. Ongoing task user profile broken.

--- seeVcam/userprofile/urls.py
+++ seeVcam/userprofile/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import patterns, url
-from views import UserProfileView
+from views import UserProfileView, UserProfileUpdate, UserProfileSettings
 
 urlpatterns = patterns('',
                        url(r'^$', UserProfileView.as_view(), name='user_profile'),
+                       url(r'^(?P<pk>\d+)/update/$', UserProfileUpdate.as_view(), name='profile_update'),
+                       url(r'^(?P<pk>\d+)/settings/$',UserProfileSettings.as_view(), name='profile_settings'),
 )
--- seeVcam/userprofile/views.py
+++ seeVcam/userprofile/views.py
@@ -1,11 +1,30 @@
 from django.contrib.auth.decorators import login_required
+from django.core.exceptions import PermissionDenied
+from django.http import Http404
 from django.utils.decorators import method_decorator
 from django.views.generic import TemplateView
+from django.views.generic import UpdateView
+from authentication.models import SeevcamUser
 
 
 class UserProfileView(TemplateView):
-    template_name = "profile.html"
 
     @method_decorator(login_required)
     def dispatch(self, *args, **kwargs):
         return super(UserProfileView, self).dispatch(*args, **kwargs)
+
+
+class UserProfileUpdate(UpdateView):
+    model = SeevcamUser
+    fields = ['username', 'email', 'first_name', 'last_name', 'job_title']
+    template_name = 'profile_update.html'
+
+    def get_object(self, *args, **kwargs):
+        obj = super(UserProfileUpdate, self).get_object(*args, **kwargs)
+        if not obj == self.request.user:
+            raise Http404
+        return obj
+
+
+class UserProfileSettings(TemplateView):
+    template_name = 'profile_setting.html'
