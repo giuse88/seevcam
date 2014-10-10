7be043a4ca08d38ad00059130990bfb985d66d53
Giuseppe <giuse88@gmail.com>
2014-10-10 21:27:41 +0100

    Added notes app structure.

---
+++ seeVcam/notes/serializer.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from notes.models import Notes
+
+
+class NoteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Notes
+        fields = ('text_content',)
