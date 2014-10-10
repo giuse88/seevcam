4452543ec5fdd71e1f364046cecc42148237184e
Giuseppe <giuse88@gmail.com>
2014-10-10 21:28:45 +0100

    Missing files

---
+++ seeVcam/notes/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+ # TODO : THis file contains both REST view and nomal view
+
+# Rest view is going to be just a PUT Request on the following url :
+   # PUT : interview/<id>/notes
+
+# Normal view only renders the content
