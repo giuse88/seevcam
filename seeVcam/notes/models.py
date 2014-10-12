12ec6e8fed6354d216bb1f6abb3bc5523a4f17c0
Giuseppe <giuse88@gmail.com>
2014-10-12 18:39:03 +0200

    Rest view to be tested.

--- seeVcam/notes/models.py
+++ seeVcam/notes/models.py
@@ -6,3 +6,4 @@ from interviews.models import Interview
 class Notes(models.Model):
     text_content = models.TextField(max_length=1000, null=False, blank=True, default='')
     interview = models.ForeignKey(Interview, null=True, blank=True)
+
---
+++ seeVcam/notes/urls.py
@@ -0,0 +1,19 @@
+from django.conf.urls import patterns, url
+from notes.views import NotesRESTView
+
+"""
+REST requests
+"""
+rest_url = patterns('',
+                      url(r'^/(?P<interview_id>[0-9]+)/notes/$', NotesRESTView.as_view()),)
+
+"""
+HTML requests
+"""
+default_url = patterns('',)
+
+
+urlpatterns = rest_url + default_url
+
+
+
