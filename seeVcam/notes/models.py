76b6ec0a71500d2d862dfb297982ad6100fb306e
Giuseppe <giuse88@gmail.com>
2014-10-14 22:21:11 +0100

    Revert "Notes app"

--- seeVcam/notes/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-from interviews.models import Interview
-
-
-class Notes(models.Model):
-    text_content = models.TextField(max_length=1000, null=False, blank=True, default='')
-    interview = models.ForeignKey(Interview, null=True, blank=True)
--- seeVcam/notes/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-
- # TODO : THis file contains both REST view and nomal view
-
-# Rest view is going to be just a PUT Request on the following url :
-   # PUT : interview/<id>/notes
-
-# Normal view only renders the content
