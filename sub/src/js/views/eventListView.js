e061e65e3efbe8ae4c0768fc69fc44d747c3493b
Pirvu Doru Lucian <doru.pirvu@agilefreaks.com>
2014-12-23 21:18:48 +0200

    append incoming events to the bottom of the event list

--- sub/src/js/views/eventListView.js
+++ sub/src/js/views/eventListView.js
@@ -14,15 +14,6 @@ define(function (require) {
       this.listenTo(this.collection, 'add', this.addEvent, this);
     },
 
-    renderSubView: function (subView, selector) {
-      var $container = this.$(selector);
-
-      if ($container.length) {
-        $container.prepend(subView.$el);
-        subView.render();
-      }
-    },
-
     addEvent: function (event) {
       this.hasSubView(this.listSelector, new EventView({model: event, el: $('<li></li>')}));
     }
