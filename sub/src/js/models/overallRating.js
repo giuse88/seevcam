9a5312afe60bd75c30fe5057df56e3efc2eb9b30
Pirvu Doru Lucian <doru.pirvu@agilefreaks.com>
2014-12-26 11:21:26 +0200

    autosaves overall rating on user click

--- sub/src/js/models/overallRating.js
+++ sub/src/js/models/overallRating.js
@@ -1,11 +1,16 @@
 define(function (require) {
   var BaseModel = require('baseModel');
+  var AutoSaveBehavior = require('behaviors/autosaveBehavior');
 
   return BaseModel.extend({
     defaults: {
       id: null,
       question: null,
       rating: null
+    },
+
+    initBehaviors: function () {
+      this.attachBehavior(new AutoSaveBehavior());
     }
   })
 });
