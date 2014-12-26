272cd308b7656e2ea0aebfe24923f1acdfe0d785
Pirvu Doru Lucian <doru.pirvu@agilefreaks.com>
2014-12-26 11:16:35 +0200

    adds highlight overall rating values

--- sub/src/js/views/overallRatingView.js
+++ sub/src/js/views/overallRatingView.js
@@ -1,7 +1,48 @@
 define(function (require) {
   var BaseView = require('baseView');
+  var _ = require('underscore');
 
   return BaseView.extend({
-    template: require('text!templates/overall-rating.html')
+    template: require('text!templates/overall-rating.html'),
+
+    events: {
+      'mouseover [data-rating-value]': 'onMouseEnterRating',
+      'mouseleave [data-rating-value]': 'onMouseLeaveRating'
+    },
+
+    postRender: function () {
+      this.highlightRating(this.model.get('rating'));
+    },
+
+    onMouseEnterRating: function (e) {
+      var ratingValue = parseInt($(e.currentTarget).data('rating-value'));
+      this.highlightRating(ratingValue);
+    },
+
+    onMouseLeaveRating: function () {
+      this.highlightRating(this.model.get('rating'));
+    },
+
+    highlightRating: function (ratingValue) {
+      ratingValue = ratingValue || 0;
+
+      var ratingClass;
+      if (ratingValue < 3) {
+        ratingClass = 'negative';
+      } else if (ratingValue == 3) {
+        ratingClass = 'neuter';
+      } else {
+        ratingClass = 'positive';
+      }
+
+      this.$('[data-rating-value]').each(function () {
+        var thisRating = parseInt(this.getAttribute('data-rating-value'));
+        if (thisRating <= ratingValue) {
+          this.setAttribute('class', ratingClass);
+        } else {
+          this.setAttribute('class', 'empty');
+        }
+      });
+    }
   });
 });
