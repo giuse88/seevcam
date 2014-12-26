979ca05c560890eabf4437e352adab6b458342cd
Pirvu Doru Lucian <doru.pirvu@agilefreaks.com>
2014-12-27 00:42:14 +0200

    adds question view

--- sub/src/js/views/review/answerView.js
+++ sub/src/js/views/review/answerView.js
@@ -1,7 +1,18 @@
 define(function (require) {
   var BaseView = require('baseView');
+  var QuestionView = require('views/review/questionView');
 
   return BaseView.extend({
-    template: require('text!templates/review/answer.html')
+    template: require('text!templates/review/answer.html'),
+
+    initialize: function (options) {
+      this.question = options.question;
+
+      BaseView.prototype.initialize.apply(this, arguments);
+    },
+
+    setUp: function () {
+      this.hasSubView('.question', new QuestionView({model: this.question}));
+    }
   });
 });
