06a9c6de19f5d6e79702d722b5897a65639af2ab
Giuseppe <giuse88@gmail.com>
2014-12-26 09:39:18 +0100

    Installed grunt

---
+++ Gruntfile.js
@@ -0,0 +1,39 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+
+    less: {
+      default: {
+        options: {
+          compress: true,
+          cleancss: true,
+          yuicompress: true,
+          optimization: 2,
+          sourceMap : true,
+          strictImports :true,
+          plugins: [
+//            new require('less-plugin-autoprefix')({browsers: ["last 2 versions"]}),
+//            new require('less-plugin-clean-css')({ advanced : true })
+          ]
+        },
+        files: {
+          "public/css/seevcam.css": "seeVcam/static/client/dashboard/main.less"
+        }
+      }
+    },
+
+    requirejs: {
+      compile: {
+        options: {
+          baseUrl: "seeVcam/static/",
+          mainConfigFile: "./seeVcam/static/client/dashboard/boot.js",
+          name: "app",
+          out: "public/js/seevcam.min.js"
+        }
+      }
+    }
+
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-requirejs');
+  grunt.loadNpmTasks('grunt-contrib-less');
+};
--- seevcam_website/login/urls.py
+++
@@ -1,6 +0,0 @@
-from django.conf.urls import patterns, url
-from views import LoginView
-
-urlpatterns = patterns('',
-                       url(r'^$', LoginView.as_view(), name='login'),
-)
