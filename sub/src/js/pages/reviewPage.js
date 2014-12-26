04a9cdc588ed97f2f770750566bc4445aa13e0f6
Pirvu Doru Lucian <doru.pirvu@agilefreaks.com>
2014-12-26 11:52:27 +0200

    adds answer list content to review page

--- sub/src/js/pages/reviewPage.js
+++ sub/src/js/pages/reviewPage.js
@@ -1,9 +1,14 @@
 define(function (require) {
   var BaseView = require('baseView');
   var OverallRatingListView = require('views/overallRatingListView');
+  var AnswerListView = require('views/answerListView');
 
   return BaseView.extend({
     template: require('text!templates/review-page.html'),
+
+    setUp: function () {
+      this.hasSubView('.content', new AnswerListView({collection: this.model.get('answers'), questions: this.model.get('questions')}));
+    },
 
     postRender: function () {
       this.openModal(new OverallRatingListView({collection: this.model.get('overallRatings')}), {
