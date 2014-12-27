6a3ae49c3a7e174b7691b1c424e55f2edccea93c
Giuseppe <giuse88@gmail.com>
2014-12-27 14:09:06 +0100

    Using boostrap less

--- seeVcam/static/client/dashboard/boot.js
+++ seeVcam/static/client/dashboard/boot.js
@@ -38,14 +38,11 @@ require.config({
   },
 
   shim: {
-        'backbone': {
-            //These script dependencies should be loaded before loading
-            //backbone.js
-            deps: ['underscore', 'jquery'],
-            //Once loaded, use the global 'Backbone' as the
-            //module value.
-            exports: 'Backbone'
-        },
+
+    'backbone': {
+        deps: ['underscore', 'jquery'],
+        exports: 'Backbone'
+    },
 
     "backbone.boostrap-modal" : {
       deps : ["backbone"]
