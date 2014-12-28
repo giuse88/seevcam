548daf33d526477e0a5b35ae589424e637a69e00
Pirvu Doru Lucian <doru.pirvu@agilefreaks.com>
2014-12-28 19:02:39 +0200

    fixes double save on notes

--- sub/src/js/behaviors/autosaveBehavior.js
+++ sub/src/js/behaviors/autosaveBehavior.js
@@ -6,22 +6,28 @@ define(function (require) {
       this.model = model;
 
       this.model.isDirty = false;
+      this.model.on('sync', this.markClean, this);
       this.model.on('change', this.execute, this);
     },
 
-    execute: function () {
+    execute: function (model, options) {
       var notTracked = this.get('ignore') || [];
       var changedAttributes = Object.keys(this.model.changed);
       this.model.isDirty = this.model.isDirty || _.any(changedAttributes, function (changedAttribute) {
         return !_.include(notTracked, changedAttribute);
       });
 
-      if (!this.model.isDirty) return;
+      var isFetchChange = options && options.parse;
+      if (!this.model.isDirty || isFetchChange) return;
 
       this.model.save()
         .then(_.bind(function () {
-          this.model.isDirty = false
+          this.markClean();
         }, this));
+    },
+
+    markClean: function () {
+      this.model.isDirty = false;
     }
   });
 });
