accaef5ebdead1061f4b48f55a4b64c22e85de63
Giuseppe <giuse88@gmail.com>
2014-12-29 19:34:51 +0100

    Model test

--- seeVcam/notes/tests/test_notes_model.py
+++ seeVcam/notes/tests/test_notes_model.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 from common.helpers.test_helper import create_company, create_user, create_catalogue, create_question, \
-    create_job_position, create_candidate, create_interview, create_uploaded_file
+    create_job_position, create_candidate, create_interview, create_uploaded_file, create_notes
 
 from notes.models import Notes
 
@@ -14,7 +14,6 @@ class TestNotes(TestCase):
         self.question = create_question(self.catalogue)
         self.job_position = create_job_position(self.user, self.company, create_uploaded_file(self.user))
         self.candidate = create_candidate(self.user, self.company, create_uploaded_file(self.user))
-        self.interview = create_interview(self.user, self.catalogue, self.candidate, self.job_position)
 
     def tearDown(self):
         self.user.delete()
@@ -23,12 +22,13 @@ class TestNotes(TestCase):
         self.job_position.delete()
         self.company.delete()
         self.candidate.delete()
-        self.interview.delete()
 
     def test_creation_notes_model(self):
-        notes = Notes(pk=1, text_content="text content")
+        notes = Notes(pk=1, content="text content")
         notes.save()
+        # I am creating the interview here so that the notes object gets a reference to interview object
+        create_interview(self.user, self.catalogue, self.candidate, self.job_position, notes)
         notes_from_db = Notes.objects.get(pk=1)
-        self.assertEqual(notes.text_content, notes_from_db.text_content, "Comparing text content")
+        self.assertEqual(notes.content, notes_from_db.content, "Comparing text content")
         self.assertEqual(notes.interview, notes_from_db.interview, "Comparing interview object")
 
