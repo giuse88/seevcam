c9ec05e9d21ec1671390264ac35278d9aa65035f
Giuseppe <giuse88@gmail.com>
2014-12-29 12:00:46 +0100

    test helper 2

--- seeVcam/common/helpers/test_helper.py
+++ seeVcam/common/helpers/test_helper.py
@@ -1,49 +1,65 @@
-from django.core.files.uploadedfile import SimpleUploadedFile
-from authentication.models import SeevcamUser
-from company_profile.models import Company
-from interviews.models import Interview
-from questions.models import QuestionCatalogue
 from StringIO import StringIO
 
+from django.core.files.uploadedfile import SimpleUploadedFile
 
-def create_interview_model():
-    user_1 = create_dummy_user('user_1', 'password')
-    file_path = "test-file/test-file.txt"
-    interview = Interview(pk=1, interview_owner=user_1,
-                          candidate_email="test@email.it",
-                          candidate_name="name",
-                          candidate_surname="surname",
-                          candidate_cv=file_path,
-                          interview_job_description=file_path,
-                          interview_position="position",
-                          interview_description="test",
-                          interview_datetime="2014-12-23 11:30",
-                          interview_duration=15,
-                          interview_datetime_end="2014-12-23 11:45")
-    interview.save()
-    return interview
+from authentication.models import SeevcamUser
+from company_profile.models import Company
+from interviews.models import Interview, Candidate, JobPosition
+from questions.models import QuestionCatalogue, Question
+from file_upload.models import UploadedFile
 
 
-def create_dummy_user(email, company, password='test'):
+def create_user(company, email="test@test.com", password='test'):
     user = SeevcamUser.objects.create_user(email, password, company)
     user.save()
     return user
 
 
-def create_dummy_company(name="company"):
+def create_company(name="test company"):
     company = Company(name=name)
     company.save()
     return company
 
 
-def create_dummy_catalogue(name, user):
+def create_catalogue(user, catalogue_name="test catalogue"):
     catalogue = QuestionCatalogue(catalogue_scope=QuestionCatalogue.SEEVCAM_SCOPE,
-                                  catalogue_name=name, catalogue_owner=user)
+                                  catalogue_name=catalogue_name, catalogue_owner=user)
     catalogue.save()
     return catalogue
 
 
-def create_upload_file(name="test.pdf", type='application/pdf'):
-     raw_content = StringIO('GIF87a\x01\x00\x01\x00\x80\x01\x00\x00\x00\x00ccc,\x00'
-                             '\x00\x00\x00\x01\x00\x01\x00\x00\x02\x02D\x01\x00;')
-     return SimpleUploadedFile(name, raw_content.read(), type)
+def create_question(catalogue, text="test question"):
+    question = Question(question_catalogue=catalogue, question_text=text)
+    question.save()
+    return question
+
+
+def create_file(name="test.pdf", file_type='application/pdf'):
+    raw_content = StringIO('GIF87a\x01\x00\x01\x00\x80\x01\x00\x00\x00\x00ccc,' +
+                           '\x00\x00\x00\x00\x01\x00\x01\x00\x00\x02\x02D\x01\x00;')
+    return SimpleUploadedFile(name, raw_content.read(), file_type)
+
+
+def create_uploaded_file(user):
+    uploaded_file = UploadedFile.objects.create_uploaded_file(create_file(), user)
+    uploaded_file.save()
+    return uploaded_file
+
+
+def create_candidate(user, company, cv, name="candidate name", surname="candidate surname", email="candidate email"):
+    candidate = Candidate(created_by=user, company=company, name=name, surname=surname, email=email, cv=cv)
+    candidate.save()
+    return candidate
+
+
+def create_job_position(user, company, job_description, position="test position"):
+    job_position = JobPosition(position=position, company=company, created_by=user, job_description=job_description)
+    job_position.save()
+    return job_position
+
+
+def create_interview(user, catalogue, candidate, job_position):
+    interview = Interview(status=Interview.OPEN, start='2014-12-23 11:30', end='2014-12-23 12:00', duration=30,
+                          catalogue=catalogue, owner=user, candidate=candidate, job_position=job_position)
+    interview.save()
+    return interview
