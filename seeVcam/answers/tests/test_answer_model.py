9c95da1d3ee891b87da0c7400a89db1c2eb78c3b
Giuseppe <giuse88@gmail.com>
2014-12-29 11:59:45 +0100

    answers app

--- seeVcam/answers/tests/test_answer_model.py
+++ seeVcam/answers/tests/test_answer_model.py
@@ -1,18 +1,58 @@
+from django.core.exceptions import ValidationError
+
 from django.test import TestCase
+from answers.models import Answer
 
-from common.helpers.test_helper import create_interview_model
-from notes.models import Notes
+from common.helpers.test_helper import create_user, create_company, create_catalogue, create_question, \
+    create_job_position, create_uploaded_file, create_candidate, create_interview
 
 
-class TestNotes(TestCase):
-    def set_up(self):
-        pass
+class TestAnswer(TestCase):
 
-    def test_creation_notes_model(self):
-        interview = create_interview_model()
-        notes = Notes(pk=1, text_content="text content", interview=interview)
-        notes.save()
-        notes_from_db = Notes.objects.get(pk=1)
-        self.assertEqual(notes.text_content, notes_from_db.text_content, "Comparing text content")
-        self.assertEqual(notes.interview, notes_from_db.interview, "Comparing interview object")
+    def setUp(self):
+        self.company = create_company()
+        self.user = create_user(self.company)
+        self.catalogue = create_catalogue(self.user)
+        self.question = create_question(self.catalogue)
+        self.job_position = create_job_position(self.user, self.company, create_uploaded_file(self.user))
+        self.candidate = create_candidate(self.user, self.company, create_uploaded_file(self.user))
+        self.interview = create_interview(self.user, self.catalogue, self.candidate, self.job_position)
+
+    def tearDown(self):
+        self.user.delete()
+        self.catalogue.delete()
+        self.question.delete()
+        self.job_position.delete()
+        self.company.delete()
+        self.candidate.delete()
+        self.interview.delete()
+
+    def test_create_answer_model_with_answer(self):
+        answer = Answer(pk=1, interview=self.interview, question=self.question, content='This is an answer')
+        answer.save()
+        answer_from_db = Answer.objects.get(pk=1)
+
+        self.assertEqual(answer.content, answer_from_db.content, "Comparing text content")
+        self.assertEqual(answer.interview, answer_from_db.interview, "Comparing interview object")
+
+    def test_create_answer_model_with_rating(self):
+        answer = Answer(pk=1, interview=self.interview, question=self.question, rating=3)
+        answer.save()
+        answer_from_db = Answer.objects.get(pk=1)
+
+        self.assertEqual(answer.rating, answer_from_db.rating,  "Comparing rating content")
+        self.assertEqual(answer.interview, answer_from_db.interview, "Comparing interview object")
+
+    def test_create_answer_model_with_rating_and_content(self):
+        answer = Answer(pk=1, interview=self.interview, question=self.question, rating=3, content='This is an answer')
+        answer.save()
+        answer_from_db = Answer.objects.get(pk=1)
+
+        self.assertEqual(answer.content, answer_from_db.content, "Comparing text content")
+        self.assertEqual(answer.rating, answer_from_db.rating,  "Comparing rating content")
+        self.assertEqual(answer.interview, answer_from_db.interview, "Comparing interview object")
+
+    def test_create_answer_with_invalid_rating(self):
+        answer = Answer(pk=1, interview=self.interview, question=self.question, rating=1000)
+        self.assertRaises(ValidationError, answer.full_clean)
 
