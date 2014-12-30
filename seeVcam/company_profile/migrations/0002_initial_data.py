9f3bf43f47bfc229b3cdbaba87ab4fc9963ccca8
fadykalo <fadykalo@gmail.com>
2014-12-30 18:59:57 +0100

    initial data load for users

--- seeVcam/company_profile/migrations/0002_initial_data.py
+++ seeVcam/company_profile/migrations/0002_initial_data.py
@@ -4,11 +4,17 @@ from __future__ import unicode_literals
 from django.db import models, migrations
 
 
-class Migration(migrations.Migration):
+def populate_company(apps, schema_editor):
+    Company = apps.get_model("company_profile", "Company")
+    db_alias = schema_editor.connection.alias
+    Company.objects.using(db_alias).create(name="seevcam")
 
+
+class Migration(migrations.Migration):
     dependencies = [
         ('company_profile', '0001_initial'),
     ]
 
     operations = [
+        migrations.RunPython(populate_company),
     ]
--- seeVcam/userprofile/migrations/0002_initial_data.py
+++ seeVcam/userprofile/migrations/0002_initial_data.py
@@ -4,11 +4,20 @@ from __future__ import unicode_literals
 from django.db import models, migrations
 
 
-class Migration(migrations.Migration):
+def populate_profiles(apps, schema_editor):
+    UserNotifications = apps.get_model("userprofile", "UserNotifications")
+    db_alias = schema_editor.connection.alias
+    UserNotifications.objects.using(db_alias).bulk_create([
+        UserNotifications(notification_when_room_is_open=1, notification_day_before=0, notification_hour_before=0),
+        UserNotifications(notification_when_room_is_open=1, notification_day_before=0, notification_hour_before=0)
+    ])
 
+
+class Migration(migrations.Migration):
     dependencies = [
         ('userprofile', '0001_initial'),
     ]
 
     operations = [
+        migrations.RunPython(populate_profiles),
     ]
