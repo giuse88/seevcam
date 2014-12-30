df58cdc919b3fe34c4ea05f8a1dfcdd1acd43a47
fadykalo <fadykalo@gmail.com>
2014-12-30 20:24:18 +0100

    added initial data load for questions

--- seeVcam/authentication/migrations/0002_initial_data.py
+++ seeVcam/authentication/migrations/0002_initial_data.py
@@ -4,31 +4,32 @@ from __future__ import unicode_literals
 from django.db import models, migrations
 import django.contrib.auth.hashers as hasher
 
-def populate_superuser(apps, schema_editor):
-    seevcam_user = apps.get_model("authentication", "SeevcamUser")
-    company = apps.get_model("company_profile", "Company")
-    notifications = apps.get_model("userprofile", "UserNotifications")
-    db_alias = schema_editor.connection.alias
-    seevcam_company = company.objects.get(id=1)
-    seevcam_user.objects.using(db_alias).create(is_staff=True, is_superuser=True,
-                                                password=hasher.make_password(password="admin"),
-                                                email="admin@admin.com",
-                                                first_name="Admin",
-                                                last_name="Istrator",
-                                                country='GB',
-                                                timezone='Europe/London',
-                                                company=seevcam_company,
-                                                notifications=notifications.objects.get(id=1))
 
-    seevcam_user.objects.using(db_alias).create(is_staff=True,
-                                                password=hasher.make_password(password="fady"),
-                                                email="fady@kalo.com",
-                                                first_name="fady",
-                                                last_name="kalo",
-                                                country='GB',
-                                                timezone='Europe/London',
-                                                company=seevcam_company,
-                                                notifications=notifications.objects.get(id=2))
+def populate_superuser(apps, schema_editor):
+    SeevcamUser = apps.get_model("authentication", "SeevcamUser")
+    Company = apps.get_model("company_profile", "Company")
+    Notifications = apps.get_model("userprofile", "UserNotifications")
+    db_alias = schema_editor.connection.alias
+    seevcam_company = Company.objects.get(id=1)
+    SeevcamUser.objects.using(db_alias).create(is_staff=True, is_superuser=True,
+                                               password=hasher.make_password(password="admin"),
+                                               email="admin@admin.com",
+                                               first_name="Admin",
+                                               last_name="Istrator",
+                                               country='GB',
+                                               timezone='Europe/London',
+                                               company=seevcam_company,
+                                               notifications=Notifications.objects.get(id=1))
+
+    SeevcamUser.objects.using(db_alias).create(is_staff=True,
+                                               password=hasher.make_password(password="fady"),
+                                               email="fady@kalo.com",
+                                               first_name="fady",
+                                               last_name="kalo",
+                                               country='GB',
+                                               timezone='Europe/London',
+                                               company=seevcam_company,
+                                               notifications=Notifications.objects.get(id=2))
 
 
 class Migration(migrations.Migration):
--- seeVcam/questions/migrations/0002_initial_data.py
+++ seeVcam/questions/migrations/0002_initial_data.py
@@ -4,24 +4,38 @@ from __future__ import unicode_literals
 from django.db import models, migrations
 
 
-def populate_catalogs(apps, schema_editor): pass
-    # Catalog = apps.get_model("questions", "QuestionCatalogue")
-    # User = apps.get_model("authentication", "SeevcamUser")
-    # db_alias = schema_editor.connection.alias
-    # seevcam = User.objects.get(id=1)
-    # print(seevcam)
-    # Catalog.objects.using(db_alias).bulk_create([
-    #     Catalog(catalogue_scope="SEEVCAM", catalogue_name="LOAD_1", catalogue_owner=seevcam),
-    #     Catalog(catalogue_scope="SEEVCAM", catalogue_name="LOAD_2", catalogue_owner=seevcam),
-    #     Catalog(catalogue_scope="SEEVCAM", catalogue_name="LOAD_3", catalogue_owner=seevcam)
-    # ])
+def populate_catalogs(apps, schema_editor):
+    Catalog = apps.get_model("questions", "QuestionCatalogue")
+    User = apps.get_model("authentication", "SeevcamUser")
+    db_alias = schema_editor.connection.alias
+    seevcam = User.objects.get(email="admin@admin.com")
+    Catalog.objects.using(db_alias).bulk_create([
+        Catalog(catalogue_scope="SEEVCAM", catalogue_name="Catalog_1", catalogue_owner=seevcam),
+        Catalog(catalogue_scope="SEEVCAM", catalogue_name="Catalog_2", catalogue_owner=seevcam),
+        Catalog(catalogue_scope="SEEVCAM", catalogue_name="Catalog_3", catalogue_owner=seevcam)
+    ])
+
+
+def populate_questions(apps, schema_editor):
+    Catalog = apps.get_model("questions", "QuestionCatalogue")
+    Question = apps.get_model("questions", "Question")
+    User = apps.get_model("authentication", "SeevcamUser")
+    db_alias = schema_editor.connection.alias
+    seevcam = User.objects.get(email="admin@admin.com")
+    seevcam_catalogs = Catalog.objects.filter(catalogue_owner=seevcam)
+    for i, catalog in enumerate(seevcam_catalogs):
+        for j in range(1, 4):
+            text = "question_" + str(i + 1) + "." + str(j)
+            Question.objects.using(db_alias).create(question_text=text, question_catalogue=catalog)
 
 
 class Migration(migrations.Migration):
     dependencies = [
         ('questions', '0001_initial'),
+        ('authentication', '0002_initial_data'),
     ]
 
     operations = [
         migrations.RunPython(populate_catalogs),
+        migrations.RunPython(populate_questions),
     ]
