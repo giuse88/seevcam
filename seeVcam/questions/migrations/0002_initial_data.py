de1779fe9f8db8e12cc532ad4002ffb99038286c
fadykalo <fadykalo@gmail.com>
2014-12-31 13:34:55 +0100

    catalogs load from YAML format

--- seeVcam/questions/migrations/0002_initial_data.py
+++ seeVcam/questions/migrations/0002_initial_data.py
@@ -2,31 +2,40 @@
 from __future__ import unicode_literals
 
 from django.db import models, migrations
+import yaml
+
+document = """
+Catalog_1:
+ - question_1.1
+ - question_1.2
+ - question_1.3
+Catalog_2:
+ - question_2.1
+ - question_2.2
+ - question_2.3
+Catalog_3:
+ - question_3.1
+ - question_3.2
+ - question_3.3
+"""
 
 
 def populate_catalogs(apps, schema_editor):
-    Catalog = apps.get_model("questions", "QuestionCatalogue")
-    User = apps.get_model("authentication", "SeevcamUser")
-    db_alias = schema_editor.connection.alias
-    seevcam = User.objects.get(email="admin@admin.com")
-    Catalog.objects.using(db_alias).bulk_create([
-        Catalog(catalogue_scope="SEEVCAM", catalogue_name="Catalog_1", catalogue_owner=seevcam),
-        Catalog(catalogue_scope="SEEVCAM", catalogue_name="Catalog_2", catalogue_owner=seevcam),
-        Catalog(catalogue_scope="SEEVCAM", catalogue_name="Catalog_3", catalogue_owner=seevcam)
-    ])
-
-
-def populate_questions(apps, schema_editor):
     Catalog = apps.get_model("questions", "QuestionCatalogue")
     Question = apps.get_model("questions", "Question")
     User = apps.get_model("authentication", "SeevcamUser")
     db_alias = schema_editor.connection.alias
     seevcam = User.objects.get(email="admin@admin.com")
-    seevcam_catalogs = Catalog.objects.filter(catalogue_owner=seevcam)
-    for i, catalog in enumerate(seevcam_catalogs):
-        for j in range(1, 4):
-            text = "question_" + str(i + 1) + "." + str(j)
-            Question.objects.using(db_alias).create(question_text=text, question_catalogue=catalog)
+    catalogs_to_load = yaml.load(document)
+    print()
+    print(type(catalogs_to_load))
+    print(catalogs_to_load)
+    for k, v in catalogs_to_load.items():
+        cat = Catalog.objects.using(db_alias).create(
+            catalogue_scope="SEEVCAM", catalogue_name=k, catalogue_owner=seevcam
+        )
+        for q in v:
+            Question.objects.using(db_alias).create(question_text=q, question_catalogue=cat)
 
 
 class Migration(migrations.Migration):
@@ -37,5 +46,4 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.RunPython(populate_catalogs),
-        migrations.RunPython(populate_questions),
     ]
