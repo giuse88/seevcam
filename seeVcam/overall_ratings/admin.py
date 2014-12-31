93c4305fd4ad4e8c7d9c8f6bf2c3b20075daf745
Giuseppe <giuse88@gmail.com>
2014-12-31 17:41:02 +0100

    overall_ratings models

--- seeVcam/overall_ratings/admin.py
+++ seeVcam/overall_ratings/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import OverallRatingQuestion
 
-# Register your models here.
+
+class OverallRatingQuestionAdmin(admin.ModelAdmin):
+    fields = ('id', 'question')
+
+admin.site.register(OverallRatingQuestion, OverallRatingQuestionAdmin)
--- seeVcam/overall_ratings/models.py
+++ seeVcam/overall_ratings/models.py
@@ -1,3 +1,33 @@
+from django.core.exceptions import ValidationError
 from django.db import models
+from common.mixins.model import UpdateCreateTimeStamp
+from interviews.models import Interview
 
-# Create your models here.
+
+class OverallRatingQuestion(models.model):
+    question = models.TextField(null=False, blank=False, db_column='overall_rating_question')
+
+    class Meta:
+        verbose_name = 'overall_rating_question'
+        verbose_name_plural = 'overall_rating_questions'
+        db_table = "overall_rating_questions"
+
+
+
+class OverallRating(UpdateCreateTimeStamp):
+
+    interview = models.ForeignKey(Interview, null=False, blank=False, db_column='interview_id')
+    question = models.ForeignKey(OverallRatingQuestion, null=False, blank=False, db_column='question_id')
+    rating = models.PositiveSmallIntegerField(validators=[validate_rating],
+                                              null=True, blank=True, db_column='rating')
+
+    class Meta:
+        verbose_name = 'overall_ratings'
+        verbose_name_plural = 'overall_ratings'
+        db_table = "overall_rating"
+
+
+# I don't like it to have a validation function which is not contained within the model where it is used
+def validate_rating(value):
+    if value < 1 or value > 5:
+        raise ValidationError("Error : " + str(value) + " is a incorrect rating.")
