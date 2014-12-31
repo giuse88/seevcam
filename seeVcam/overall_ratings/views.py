cd06a58f96453ad4d417ea5bd5afa50e20e0f338
Giuseppe <giuse88@gmail.com>
2014-12-31 19:50:41 +0100

    Interview view rating

--- seeVcam/overall_ratings/views.py
+++ seeVcam/overall_ratings/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework.generics import ListAPIView, UpdateAPIView
+from overall_ratings.models import OverallRating
+from overall_ratings.serializer import OverallRatingSerializer
 
-# Create your views here.
+
+class ListOverviewRatting(ListAPIView):
+    serializer_class = OverallRatingSerializer
+    model = OverallRating
+
+    def get_queryset(self):
+        return OverallRating.objects.filter(interview=self.kwargs['interview_id'])
+
+
+class UpdateOverviewRatting(UpdateAPIView):
+    serializer_class = OverallRatingSerializer
+    model = OverallRating
+
+    def pre_save(self, obj):
+        obj.interview_id = self.kwargs['interview_id']
+
+    def get_queryset(self):
+        return OverallRating.objects.filter(interview=self.kwargs['interview_id'],
+                                            pk=self.kwargs['pk'])
