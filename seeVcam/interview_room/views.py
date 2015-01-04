b2f3e56d8a522dcd76266cce9511bc4c56c3a363
Giuseppe <giuse88@gmail.com>
2015-01-04 13:26:52 +0100

    Experiments

--- seeVcam/interview_room/views.py
+++ seeVcam/interview_room/views.py
@@ -5,19 +5,25 @@ from opentok import OpenTok, Roles
 
 class InterviewRoomView(TemplateView):
     template_name = "interview-room.html"
-
+    test = False
     # this should be valid only for the interviewer
     # @method_decorator(login_required)
     # def dispatch(self, *args, **kwargs):
     #     return super(InterviewRoomView, self).dispatch(*args, **kwargs)
 
     def get_context_data(self, **kwargs):
+        InterviewRoomView.test = not InterviewRoomView.test
         context = super(InterviewRoomView, self).get_context_data(**kwargs)
         # this should be a singleton
         opentok = OpenTok(settings.OPENTOK_API_KEY, settings.OPENTOK_SECRET)
         # session = opentok.create_session()
         # context['session_id'] = session.session_id
+        context['test'] = self.test
         context['api_key'] = settings.OPENTOK_API_KEY
         context['session_id'] = "1_MX40NTExOTk3Mn5-MTQyMDI3NzYxMzI1NH5zek80L1owVkRadGVRMS9peUZQR2dKa0l-UH4"
-        context['token'] = opentok.generate_token(session_id=context['session_id'], role=Roles.publisher)
+        context['token'] = opentok.generate_token(session_id=context['session_id'],
+                                                  data="test=" + str(InterviewRoomView.test),
+                                                  role=Roles.publisher)
+        print(self.test)
+        print(context['token'])
         return context
