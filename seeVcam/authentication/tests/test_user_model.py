c306eee28e3310f483b94dde1fd0d9fe60f8c6db
Giuseppe <giuse88@gmail.com>
2015-01-13 22:49:56 +0000

    Fixed authentication tests

--- seeVcam/authentication/tests/test_user_model.py
+++ seeVcam/authentication/tests/test_user_model.py
@@ -1,18 +1,13 @@
 from django.test import TestCase
 from authentication.models import SeevcamUser
+from common.helpers.test_helper import create_company
 
 
 class UserTest(TestCase):
-    def setUp(self):
-        pass
 
     def test_creation_of_a_seevcamuser(self):
-        user = SeevcamUser.objects.create_user('test_username', email=None, password='test_pwd',
-                                               job_title='test_job_title', first_name='test_first_name',
-                                               last_name='test_last_name', pk=1)
+        company = create_company()
+        user = SeevcamUser.objects.create_user("test@test.com",  'test_pwd', company)
         user.save()
-        user_from_db = SeevcamUser.objects.get(pk=1)
-        self.assertEqual('test_username', user_from_db.username)
-        self.assertEqual('test_job_title', user_from_db.job_title)
-        self.assertEqual('test_first_name', user_from_db.first_name)
-        self.assertEqual('test_last_name', user_from_db.last_name)
+        user_from_db = SeevcamUser.objects.get(pk=user.id)
+        self.assertEqual('test@test.com', user_from_db.email)
