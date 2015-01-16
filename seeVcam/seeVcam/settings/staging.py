e36f2dcb9ff4eecf9cb24a69d63407f2e2be1fdd
fadykalo <fadykalo@gmail.com>
2015-01-16 18:07:15 +0000

    added STATIC_ROOT to staging settings

--- seeVcam/seeVcam/settings/staging.py
+++ seeVcam/seeVcam/settings/staging.py
@@ -21,6 +21,8 @@ def get_secret(setting, secrets=secrets):
         error_msg = "Set the {0} environment variable".format(setting)
         raise ImproperlyConfigured(error_msg)
 
+STATIC_ROOT = get_secret("STATIC_ROOT")
+
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = get_secret("SECRET_KEY")
 
