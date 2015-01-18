cfa63cdf077c9356665b53773a8e9b92a570bbef
fadykalo <fadykalo@gmail.com>
2015-01-18 19:10:33 +0000

    fixed gunicorn script

--- deploy/tasks/gunicorn_reload.js
+++ deploy/tasks/gunicorn_reload.js
@@ -9,9 +9,8 @@ module.exports = function (grunt) {
 	grunt.registerTask('gunicorn_reload', 'reloads gunicorn process', function () {
 		var done = this.async();
 		grunt.shipit.remote(
-				'cd ' + grunt.config('shipit.options.current')+'seeVcam '
-				+ '&& workon seevcam '
-				+ '&& kill -HUP `cat ../tmp/gunicorn.pid`'
+				'cd ' + grunt.config('shipit.options.current')+' '
+				+'&& cat tmp/gunicorn.pid | xargs kill -HUP'
 			, done);
 	});
 };
