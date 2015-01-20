0487144b49b2660930cd9712352d1af5c36b18fa
fadykalo <fadykalo@gmail.com>
2015-01-20 23:06:52 +0000

    added /images for the client to the collectstatic paths

--- seeVcam/seeVcam/settings/staging.py
+++ seeVcam/seeVcam/settings/staging.py
@@ -27,7 +27,8 @@ STATIC_ROOT = '/home/seevcam/app/seevcam/current/public/'
 STATIC_URL = '/'
 STATICFILES_DIRS = [
     ('bootstrap/css', os.path.join(BASE_DIR, 'static/bower_components/bootstrap/dist/css/')),
-    ('client/login', os.path.join(BASE_DIR, 'static/client/login/css/'))
+    ('client/login', os.path.join(BASE_DIR, 'static/client/login/css/')),
+    ('static/images/UI', os.path.join(BASE_DIR, 'static/images/UI/'))
 ]
 
 # SECURITY WARNING: keep the secret key used in production secret!
