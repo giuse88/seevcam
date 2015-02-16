54f752d5e2228da74c28d59bb416ce470dfd57d5
fadykalo <fadykalo@gmail.com>
2015-02-16 20:54:20 +0000

    added billing page

--- seeVcam/userprofile/urls.py
+++ seeVcam/userprofile/urls.py
@@ -2,13 +2,14 @@ from django.conf.urls import patterns, url
 from django.core.urlresolvers import reverse_lazy
 from django.views.generic import RedirectView
 
-from .views import UserProfileUpdate, UserProfileNotifications, UserProfileSettings, UserProfileIntegration
+from .views import UserProfileUpdate, UserProfileNotifications, UserProfileSettings, UserProfileIntegration, UserProfileBilling
 
 html_patterns = patterns('',
                          url(r'^update/$', UserProfileUpdate.as_view(), name='profile_update'),
                          url(r'^notifications/$', UserProfileNotifications.as_view(), name='profile_notifications'),
                          url(r'^settings/$', UserProfileSettings.as_view(), name='profile_settings'),
                          url(r'^integrations/$', UserProfileIntegration.as_view(), name='profile_integration'),
+                         url(r'^billing/$', UserProfileBilling.as_view(), name='profile_billing'),
                          url(r'^$', RedirectView.as_view(url=reverse_lazy('profile_update')), name='user_profile'),
                          )
 
