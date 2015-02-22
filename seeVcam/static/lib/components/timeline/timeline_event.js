97ffd523c384694b2441fc6fe583be222fd171ec
Giuseppe <giuse88@gmail.com>
2015-02-22 19:21:05 +0000

    timeline working with time

--- seeVcam/static/lib/components/timeline/timeline_event.js
+++ seeVcam/static/lib/components/timeline/timeline_event.js
@@ -1,10 +1,27 @@
 define(function (require) {
 
   var BaseView = require('baseView');
+  var Moment = require("moment");
 
   return BaseView.extend({
     template: require('text!./templates/timeline_event.html'),
-    className : "cd-timeline-block"
-  });
+    className : "cd-timeline-block",
 
+    initialize : function (options) {
+      this.options = options;
+    },
+
+    getRenderContext : function (){
+      return {
+        time : this.getRelativeTime()
+      };
+    },
+
+    getRelativeTime : function () {
+      var interviewStart = new Moment(this.options.interview.get("start"));
+      var eventInstant = new Moment(this.model.get("timestamp"))
+      return Moment(eventInstant.diff(interviewStart)).format("m[m] s[s]");
+    }
+
+  });
 });
