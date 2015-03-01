dd43eceedf3b39ce7e743b04449f76bd325a1a28
Giuseppe <giuse88@gmail.com>
2015-03-01 13:03:17 +0000

    Add admin app for interview

--- seeVcam/interviews/admin.py
+++ seeVcam/interviews/admin.py
@@ -1,8 +1,24 @@
 from django.contrib import admin
 from interviews.models import Interview, Candidate, JobPosition
 
-admin.site.register(Interview)
-admin.site.register(Candidate)
-admin.site.register(JobPosition)
+
+class CandidateAdmin(admin.ModelAdmin):
+    fields = ('name', 'surname', 'email', 'cv')
+    list_display = ('id',) + fields
+
+
+class JobPositionAdmin(admin.ModelAdmin):
+    fields = ('position', 'job_description')
+    list_display = ('id',) + fields
+
+
+class InterviewAdmin(admin.ModelAdmin):
+    fields = ('status', 'start', 'end', 'catalogue', 'owner', 'job_position', 'candidate',
+              'session_id', 'token', 'overall_score')
+    list_display = ('id',) + fields
+
+admin.site.register(Interview, InterviewAdmin)
+admin.site.register(Candidate, CandidateAdmin)
+admin.site.register(JobPosition, JobPositionAdmin)
 
 
