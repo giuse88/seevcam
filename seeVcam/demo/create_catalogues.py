8f572ea7b06ceeeb3744f627b3a0893c903bd04a
Giuseppe <giuse88@gmail.com>
2015-03-07 17:08:23 +0000

    demo catalogues

---
+++ seeVcam/demo/create_catalogues.py
@@ -0,0 +1,38 @@
+import os
+import yaml
+from questions.models import QuestionCatalogue, Question
+
+
+BASE = os.path.dirname(os.path.abspath(__file__))
+
+
+def create_catalogue(name, user):
+    return QuestionCatalogue.objects.create(
+        catalogue_scope="PRIVATE",
+        catalogue_name=name,
+        catalogue_owner=user)
+
+
+def create_question(question, cat):
+    return Question.objects.create(
+        question_text=question,
+        question_catalogue=cat)
+
+
+def populate_catalogs(user):
+    document = open(os.path.join(BASE, 'fixtures/catalogs.yml'), 'r')
+    catalogs_to_load = yaml.load(document)
+    catalogues = []
+
+    for catalogue, questions in catalogs_to_load.items():
+        cat = create_catalogue(catalogue, user)
+        catalogues.append(cat)
+        for question in questions:
+            create_question(question, cat)
+
+    return catalogues
+
+
+def create_catalogues(user):
+    user.delete_catalogues()
+    return populate_catalogs(user)
--- seeVcam/demo/create_demo.py
+++ seeVcam/demo/create_demo.py
@@ -1,38 +1,5 @@
-import os
-
-import yaml
-
-from questions.models import QuestionCatalogue, Question
 from .uploaded_files import file_upload
-
-
-BASE = os.path.dirname(os.path.abspath(__file__))
-
-
-def create_catalogues(user):
-    user.delete_catalogue()
-    return populate_catalogs(user)
-
-
-def populate_catalogs(user):
-    document = open(os.path.join(BASE, 'fixtures/catalogues.yml'), 'r')
-    catalogs_to_load = yaml.load(document)
-    catalogues = []
-
-    for catalogue, questions in catalogs_to_load.items():
-
-        cat = QuestionCatalogue.objects.create(
-            catalogue_scope="PRIVATE",
-            catalogue_name=catalogue,
-            catalogue_owner=user)
-        catalogues.append(cat)
-
-        for question in questions:
-            Question.objects.create(
-                question_text=question,
-                question_catalogue=cat)
-
-    return catalogues
+from .create_catalogues import create_catalogues
 
 
 def create_demo(user):
