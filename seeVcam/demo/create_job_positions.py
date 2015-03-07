054df1ea7e81645abd23f7b6fb79afbff79e2c3a
Giuseppe <giuse88@gmail.com>
2015-03-07 18:50:16 +0000

    add job_position

--- seeVcam/demo/create_job_positions.py
+++ seeVcam/demo/create_job_positions.py
@@ -1,3 +1,22 @@
+import os
+import yaml
+from common.helpers.test_helper import create_job_position
+from .uploaded_files import upload_job_spec
+
+
+BASE = os.path.dirname(os.path.abspath(__file__))
+
+
+def load_position(user, position, job_spec):
+    return create_job_position(user, user.company, job_spec, position)
+
 
 def create_job_positions(user):
-    pass
+    user.delete_candidates()
+    document = open(os.path.join(BASE, 'fixtures/job_positions.yml'), 'r')
+    job_positions = []
+    for position in yaml.load(document):
+        job = upload_job_spec(user)
+        job_positions.append(load_position(user, position, job))
+    return job_positions
+
