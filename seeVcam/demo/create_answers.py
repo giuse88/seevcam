d7cee253bdde56baf68a56aa36d239707b24ae9d
Giuseppe <giuse88@gmail.com>
2015-03-08 09:07:24 +0000

    Fixed notes

--- seeVcam/demo/create_answers.py
+++ seeVcam/demo/create_answers.py
@@ -1,17 +1,21 @@
 import os
+import random
 import yaml
 from common.helpers.test_helper import create_catalogue, create_question, create_answer
 
 BASE = os.path.dirname(os.path.abspath(__file__))
 
 
-def create_answers_for_report(report, questions, answers):
+def create_answers_for_report(report, catalogue, questions, answers):
     saved_answers = []
     for answer in answers:
         saved_answers.append(create_answer(report,
                                            questions[answer["question_index"]],
                                            answer["content"],
                                            answer["rating"]))
+    report.overall_score = random.randint(40, 99)/10
+    report.catalogue = catalogue
+    report.save()
 
 
 def create_answers(user, reports):
@@ -24,4 +28,4 @@ def create_answers(user, reports):
         questions.append(create_question(catalogue, answer["question"]))
 
     for report in reports:
-        create_answers_for_report(report, questions, answers)
+        create_answers_for_report(report, catalogue, questions, answers)
--- seeVcam/demo/create_notes.py
+++ seeVcam/demo/create_notes.py
@@ -1,5 +1,7 @@
 import os
+
 import yaml
+
 from common.helpers import test_helper
 
 
@@ -7,9 +9,8 @@ BASE = os.path.dirname(os.path.abspath(__file__))
 
 
 def create_notes(reports):
-    document = open(os.path.join(BASE, 'fixtures/notes.yml'), 'r')
-    notes = yaml.load(document)["notes"]
-    print(notes)
+    document = open(os.path.join(BASE, 'fixtures/notes.html'), 'r')
+    notes = document.read()
     for report in reports:
-        test_helper.create_notes(report, notes)
+        test_helper.create_notes(report, notes.replace("XXXXX", report.candidate.name))
     return notes
