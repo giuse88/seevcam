0031f5bdb5c8b9e53780902da8e259764b1cb442
Giuseppe <giuse88@gmail.com>
2015-03-08 12:42:26 +0000

    events

--- seeVcam/demo/create_answers.py
+++ seeVcam/demo/create_answers.py
@@ -1,21 +1,44 @@
-from datetime import timedelta, datetime
 import os
 import random
+from datetime import timedelta, datetime
+
 import yaml
-from common.helpers.test_helper import create_question, create_answer
-from .create_catalogues import create_catalogue
+from django.conf import settings
+
 from events.models import Event
+from .create_catalogues import create_catalogue
+from common.helpers.test_helper import create_question, create_answer
+
 
 BASE = os.path.dirname(os.path.abspath(__file__))
 
 
 def select_question(report, question, delta):
     content = '{"question_id":%d}' % question.id
-    print(report.start)
-    start = datetime.strptime(report.start, "%Y-%m-%dT%H:%M:%S.%f%z")
+    start = datetime.strptime(report.start, settings.DATE_INPUT_FORMATS[0])
     timestamp = start + timedelta(seconds=delta)
     event = Event(timestamp=timestamp, interview=report, type=Event.QUESTION_SELECTED, content=content)
     event.save()
+
+
+def rate_created(report, question, delta):
+    rate = random.randint(1, 9)
+    content = '{"rating":%d, "question_id":%d}' % (question.id, rate)
+    start = datetime.strptime(report.start, settings.DATE_INPUT_FORMATS[0])
+    timestamp = start + timedelta(seconds=delta)
+    event = Event(timestamp=timestamp, interview=report, type=Event.RATE_CREATED, content=content)
+    event.save()
+    return rate
+
+
+def rate_updated(rate, report, question, delta):
+    content = '{"new_rating":%d,"old_rating":%d, "question_id":%d}' \
+              % (question.id, random.randint(1, 9), rate)
+    start = datetime.strptime(report.start, settings.DATE_INPUT_FORMATS[0])
+    timestamp = start + timedelta(seconds=delta)
+    event = Event(timestamp=timestamp, interview=report, type=Event.RATE_UPDATED, content=content)
+    event.save()
+    return event
 
 
 def create_answers_for_report(report, catalogue, questions, answers):
@@ -25,7 +48,7 @@ def create_answers_for_report(report, catalogue, questions, answers):
                                            questions[answer["question_index"]],
                                            answer["content"],
                                            answer["rating"]))
-    report.overall_score = random.randint(40, 99)/10
+    report.overall_score = random.randint(40, 99) / 10
     report.catalogue = catalogue
     report.save()
 
@@ -34,7 +57,9 @@ def create_events_for_reports(report, questions):
     delta = 100
     for question in questions:
         select_question(report, question, delta)
-        delta += 30
+        rate = rate_created(report, question, delta + 40)
+        rate_updated(rate, report, question, delta + 90)
+        delta += 100
 
 
 def create_answers(user, reports):
